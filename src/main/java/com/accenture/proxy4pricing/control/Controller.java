a4725a8b0b6d6fccd6de0c42909427b0c902d1a9
Toth <andras.toth@accenture.com>
2019-02-25 11:56:13 +0100

    pass incoming parameters to request

--- src/main/java/com/accenture/proxy4pricing/control/Controller.java
+++ src/main/java/com/accenture/proxy4pricing/control/Controller.java
@@ -13,45 +13,146 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.lang.StringBuilder;
 import com.accenture.proxy4pricing.data.House;
 
 @RestController
 class Controller {
 
-    @CrossOrigin(origins = {"http://localhost:4200"})
+    private HashMap<String, String> params;
+
+    final String messageStart = "{\"Inputs\": {\"input1\":[{";
+    
+    final String messageEnd = "}],},\"GlobalParameters\":  {}}";
+
+    @CrossOrigin()
     @RequestMapping(value="/greeting", consumes="application/json")
     @ResponseBody
-    public String greeting(@RequestBody House body) {
-        System.out.println("Input: " + body.getId());
-        return getPrice();
+    public String greeting(@RequestBody House house) {
+        System.out.println("Input house parameters: " + house);
+        return getPrice(house);
     }
 
-    private String getPrice()
+    private String getPrice(House house)
     {
-        final String message = "{\"Inputs\":"+ 
-                "{\"input1\": "+
-                    "{"+
-                        "\"ColumnNames\": [\"make\", \"fuel\", \"doors\", \"body\", \"drive\", \"weight\", \"engine-size\", \"bhp\", \"mpg\", \"price\"], "+
-                        "\"Values\": [[\"alfa-romero\", \"value\", \"value\", \"value\", \"value\", \"0\", \"0\", \"0\", \"0\", \"0\"], [\"value\", \"value\", \"value\", \"value\", \"value\", \"0\", \"0\", \"0\", \"0\", \"0\"]]"+
-                    "}"+                    
-                "},"+ 
-            "\"GlobalParameters\": {}"+
-            "}";
 
-        final String uri = "https://ussouthcentral.services.azureml.net/workspaces/cb2531634b964c36ad54858023e650d0/services/b6d4e92b97f042fb9ee423d2a6acaf18/execute?api-version=2.0&details=true";
+        final String uri = "https://ussouthcentral.services.azureml.net/workspaces/cb2531634b964c36ad54858023e650d0/services/f098f90df7834679905ee5a312a6518e/execute?api-version=2.0&format=swagger";
         
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.setBearerAuth("uQpZ5qC4Dhrf/TA6gtwXkDTCCWLNCp5OfB1iMfqJArmvzy6v0IN4QZNTQQeyX0BFsZLIY9GeRgbYIArDLnnE8A==");
-        HttpEntity<String> entity = new HttpEntity<String>(message, headers);
+        headers.setBearerAuth("SVXG6FS82Loy71Y6epKWs32aL/Fx7SmB0CWwHiXswIWbWFfS+bmoAbvhd7rFJCDVJDHmlab6zh3TI/6UGQuS7Q==");
+        HttpEntity<String> entity = new HttpEntity<String>(assembledMessage(house), headers);
         
         ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
         
         System.out.println(result.getBody());
 
         return(result.getBody());
+    }
+
+    private String assembledMessage(House house) {
+        initParams();
+        params.put("Id", house.getId());
+        params.put("LotArea", house.getLotArea());
+        params.put("YearBuilt", house.getYearBuilt());
+        params.put("PoolArea", house.getPoolArea());
+        StringBuilder sb = new StringBuilder(messageStart);
+        params.forEach((key, value) -> {
+            sb.append("\n\"").append(key).append("\": \"").append(value).append("\",");
+        });
+        sb.append("\n\"").append("SalePrice").append("\": \"").append("0").append("\"");
+        
+        sb.append(messageEnd);
+        System.out.println("Stringbuilder: "+sb);
+
+        return sb.toString();
+    }
+
+    private void initParams() {
+        params = new HashMap<>();
+        params.put("Id","1");
+        params.put("MSSubClass","60");
+        params.put("MSZoning","RL");
+        params.put("LotFrontage","65");
+        params.put("LotArea","8450");
+        params.put("Street","Pave");
+        params.put("Alley","NA");
+        params.put("LotShape","Reg");
+        params.put("LandContour","Lvl");
+        params.put("Utilities","AllPub");
+        params.put("LotConfig","Inside");
+        params.put("LandSlope","Gtl");
+        params.put("Neighborhood","CollgCr");
+        params.put("Condition1","Norm");
+        params.put("Condition2","Norm");
+        params.put("BldgType","1Fam");
+        params.put("HouseStyle","2Story");
+        params.put("OverallQual","7");
+        params.put("OverallCond","5");
+        params.put("YearBuilt","2003");
+        params.put("YearRemodAdd","2003");
+        params.put("RoofStyle","Gable");
+        params.put("RoofMatl","CompShg");
+        params.put("Exterior1st","VinylSd");
+        params.put("Exterior2nd","VinylSd");
+        params.put("MasVnrType","BrkFace");
+        params.put("MasVnrArea","196");
+        params.put("ExterQual","Gd");
+        params.put("ExterCond","TA");
+        params.put("Foundation","PConc");
+        params.put("BsmtQual","Gd");
+        params.put("BsmtCond","TA");
+        params.put("BsmtExposure","No");
+        params.put("BsmtFinType1","GLQ");
+        params.put("BsmtFinSF1","706");
+        params.put("BsmtFinType2","Unf");
+        params.put("BsmtFinSF2","0");
+        params.put("BsmtUnfSF","150");
+        params.put("TotalBsmtSF","856");
+        params.put("Heating","GasA");
+        params.put("HeatingQC","Ex");
+        params.put("CentralAir","Y");
+        params.put("Electrical","SBrkr");
+        params.put("1stFlrSF","856");
+        params.put("2ndFlrSF","854");
+        params.put("LowQualFinSF","0");
+        params.put("GrLivArea","1710");
+        params.put("BsmtFullBath","1");
+        params.put("BsmtHalfBath","0");
+        params.put("FullBath","2");
+        params.put("HalfBath","1");
+        params.put("BedroomAbvGr","3");
+        params.put("KitchenAbvGr","1");
+        params.put("KitchenQual","Gd");
+        params.put("TotRmsAbvGrd","8");
+        params.put("Functional","Typ");
+        params.put("Fireplaces","0");
+        params.put("FireplaceQu","NA");
+        params.put("GarageType","Attchd");
+        params.put("GarageYrBlt","2003");
+        params.put("GarageFinish","RFn");
+        params.put("GarageCars","2");
+        params.put("GarageArea","548");
+        params.put("GarageQual","TA");
+        params.put("GarageCond","TA");
+        params.put("PavedDrive","Y");
+        params.put("WoodDeckSF","0");
+        params.put("OpenPorchSF","61");
+        params.put("EnclosedPorch","0");
+        params.put("3SsnPorch","0");
+        params.put("ScreenPorch","0");
+        params.put("PoolArea","0");
+        params.put("PoolQC","NA");
+        params.put("Fence","NA");
+        params.put("MiscFeature","NA");
+        params.put("MiscVal","0");
+        params.put("MoSold","2");
+        params.put("YrSold","2008");
+        params.put("SaleType","WD");
+        params.put("SaleCondition","Normal");
     }
 
 }
--- src/main/java/com/accenture/proxy4pricing/data/House.java
+++ src/main/java/com/accenture/proxy4pricing/data/House.java
@@ -1,19 +1,52 @@
 package com.accenture.proxy4pricing.data;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 public class House {
 
     private String id;
+    private String lotArea;
+    private String yearBuilt;
+    private String poolArea;
 
-    private String area;
+    public String toString() {
+        return "\n    Id: "+id+
+        "\n    Lot area: "+lotArea+
+        "\n    Pool area: "+poolArea+
+        "\n    Year built: "+yearBuilt;
 
-    private String price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setLotArea(String lotArea) {
+        this.lotArea = lotArea;
+    }
+
+    public String getLotArea() {
+        return lotArea;
+    }
+    public void setPoolArea(String poolArea) {
+        this.poolArea = poolArea;
+    }
+
+    public String getPoolArea() {
+        return poolArea;
+    }
+    public void setYearBuilt(String yearBuilt) {
+        this.yearBuilt = yearBuilt;
+    }
+
+    public String getYearBuilt() {
+        return yearBuilt;
     }
 
 }
