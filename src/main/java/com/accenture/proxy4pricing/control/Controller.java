bf4d9fe62d3ed934cc9746f546df8de7ccd62c25
Toth <andras.toth@accenture.com>
2019-03-19 17:33:35 +0100

    fix

--- src/main/java/com/accenture/proxy4pricing/control/Controller.java
+++ src/main/java/com/accenture/proxy4pricing/control/Controller.java
@@ -43,7 +43,10 @@ class Controller {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.setBearerAuth("/8fJfU4vpYQd6fuHiqPNUV2unmL+bMEg4nEFGN1OOl9Kx+StpVnomaPvbczKS4AVH5RsxceWS8ZAplDAdr51mA==");
-        HttpEntity<String> entity = new HttpEntity<String>(assembledMessage(house), headers);
+        String message = assembledMessage(house);
+        System.out.println(headers);
+        System.out.println(message);
+        HttpEntity<String> entity = new HttpEntity<String>(message, headers);
         //ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
         return("");
     }
